b1b5de6025f9a88f340b8912a217dcce5ba7cdd4
Kirk <kirk.mossey@gmail.com>
2022-12-05 23:15:17 -0500

    commit tree 4950bd9a223112a532346a56642758ab5a14592f

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution in this file!
+
+const employee = {
+    name: "John",
+   streetAddress: "123 Main Street"
+    //    street1: "123 Main Street",
+    //    street2: "apt 1",
+    //    city: "New York",
+    //    state: "NY",
+    //    zip: "10000",
+    //}
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const employee1 = {...employee}
+    
+    employee1.name = 'Sam';
+    employee1.streetAddress = '11 Broadway' 
+
+    return employee1
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee.name = 'Sam'
+    employee.streetAddress = '12 Broadway'
+
+    return employee
+}
+
+function deleteFromEmployeeByKey(employee, key, value) {
+    const employee2 = {...employee}
+
+    delete employee2.name
+
+    return employee2
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee.name
+
+    return employee
+}
